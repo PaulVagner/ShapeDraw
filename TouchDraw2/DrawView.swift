bb480dbe72776fede83b2203a749e116cd28169f
PaulVagner <prodigyzro@gmail.com>
2016-02-06 14:33:49 -0500

    Star not working

--- TouchDraw2/DrawView.swift
+++ TouchDraw2/DrawView.swift
@@ -85,44 +85,10 @@ class DrawView: UIView {
                             
                         case .Star :
                             
-                            func pointFrom(angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
+                            let shape: StarShape = .Star{
                                 
-                                return CGPointMake(radius * cos(angle) + offset.x, radius * sin(angle) + offset.y)
                             }
-                            
-                            func starPathInRect(rect: CGRect) -> UIBezierPath {
-                                let path = UIBezierPath()
-                                let starExtrusion: CGFloat = 30.0
-                                let center = CGPointMake(rect.width / 2.0, rect.height / 2.0)
-                                let pointsOnStar = 5 + arc4random() % 10
-                                var angle: CGFloat = -CGFloat(M_PI / 2.0)
-                                let angleIncrement = CGFloat(M_PI * 2.0 / Double(pointsOnStar))
-                                let radius = rect.width / 2.0
-                                
-                                var firstPoint = true
-                                
-                                for _ in 1...pointsOnStar {
-                                    
-                                    let point = pointFrom(angle, radius: radius, offset: center)
-                                    let nextPoint = pointFrom(angle + angleIncrement, radius: radius, offset: center)
-                                    let midPoint = pointFrom(angle + angleIncrement / 2.0, radius: starExtrusion, offset: center)
-           
-                                    
-                                    if firstPoint {
-                                        firstPoint = false
-                                        path.moveToPoint(point)
-                                    }
-                                    
-                                    path.addLineToPoint(midPoint)
-                                    path.addLineToPoint(nextPoint)
-                                    
-                                    angle += angleIncrement
-                                }
-                                
-                                path.closePath()
-                                CGContextFillPath(context)
-                                return path
-                            }
+                           
                         }
                     }
                     
