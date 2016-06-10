3faa06575dd219a5fee79f47c476b6152807b547
PaulVagner <prodigyzro@gmail.com>
2016-06-09 20:07:46 -0400

    Stuff

--- TouchDraw2/DrawView.swift
+++ TouchDraw2/DrawView.swift
@@ -84,6 +84,7 @@ class DrawView: UIView {
                             CGContextFillPath(context)
                             
                         case .Star :
+                            CGContextFillEllipseInRect(context, rect)
                             
                             func pointFrom(angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
                                 
@@ -199,48 +200,48 @@ class DrawView: UIView {
                             CGContextStrokePath(context)
                             
                         case .Star :
-                            
-                            func pointFrom(angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
-                                
-                            return CGPointMake(radius * cos(angle) + offset.x, radius * sin(angle) + offset.y)
-                            }
-                            
-                            func starPathInRect(rect: CGRect) -> UIBezierPath {
-                                let path = UIBezierPath()
-                                
-                                let starExtrusion: CGFloat = 20.0
-                                
-                                let center = CGPointMake(rect.width / 2.0, rect.height / 2.0)
-                                
-                                let pointsOnStar = 5 + arc4random() % 10
-                                
-                                var angle: CGFloat = -CGFloat(M_PI / 2.0)
-                                let angleIncrement = CGFloat(M_PI * 2.0 / Double(pointsOnStar))
-                                let radius = rect.width / 2.0
-                                
-                                var firstPoint = true
-                                
-                                for i in 1...pointsOnStar {
-                                    
-                                    let point = pointFrom(angle, radius: radius, offset: center)
-                                    let nextPoint = pointFrom(angle + angleIncrement, radius: radius, offset: center)
-                                    let midPoint = pointFrom(angle + angleIncrement / 2.0, radius: starExtrusion, offset: center)
-                                    
-                                    if firstPoint {
-                                        firstPoint = false
-                                        path.moveToPoint(point)
-                                    }
-                                    
-                                    path.addLineToPoint(midPoint)
-                                    path.addLineToPoint(nextPoint)
-                                    
-                                    angle += angleIncrement
-                                }
-                                
-                                path.closePath()
-                                CGContextStrokePath(context)
-                                return path
-                            }
+                            CGContextFillEllipseInRect(context, rect)
+//                            func pointFrom(angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
+//                                
+//                            return CGPointMake(radius * cos(angle) + offset.x, radius * sin(angle) + offset.y)
+//                            }
+//                            
+//                            func starPathInRect(rect: CGRect) -> UIBezierPath {
+//                                let path = UIBezierPath()
+//                                
+//                                let starExtrusion: CGFloat = 20.0
+//                                
+//                                let center = CGPointMake(rect.width / 2.0, rect.height / 2.0)
+//                                
+//                                let pointsOnStar = 5 + arc4random() % 10
+//                                
+//                                var angle: CGFloat = -CGFloat(M_PI / 2.0)
+//                                let angleIncrement = CGFloat(M_PI * 2.0 / Double(pointsOnStar))
+//                                let radius = rect.width / 2.0
+//                                
+//                                var firstPoint = true
+//                                
+//                                for i in 1...pointsOnStar {
+//                                    
+//                                    let point = pointFrom(angle, radius: radius, offset: center)
+//                                    let nextPoint = pointFrom(angle + angleIncrement, radius: radius, offset: center)
+//                                    let midPoint = pointFrom(angle + angleIncrement / 2.0, radius: starExtrusion, offset: center)
+//                                    
+//                                    if firstPoint {
+//                                        firstPoint = false
+//                                        path.moveToPoint(point)
+//                                    }
+//                                    
+//                                    path.addLineToPoint(midPoint)
+//                                    path.addLineToPoint(nextPoint)
+//                                    
+//                                    angle += angleIncrement
+//                                }
+//                                
+//                                path.closePath()
+//                                CGContextStrokePath(context)
+//                                return path
+//                            }
 
                         }
                         
